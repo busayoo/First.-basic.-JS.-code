7b42def36756bda3d8c1bf0fdf253c0bb08fd76b
busayoo <103973492+busayoo@users.noreply.github.com>
2022-06-22 14:47:32 +0100

    Basic Javascript Code

---
+++ script.js
@@ -0,0 +1,8 @@
+// var Name = prompt("Enter Your Name")
+// var Height = prompt("What is Your Height?")
+// var Country = prompt("What is your Country of Origin?")
+
+// document.write(Name + "<br>" + Height + "<br>" + Country);
+alert("My Name Is Jasmine");
+alert("I am 168cm Tall");
+alert("I am From Canada");
